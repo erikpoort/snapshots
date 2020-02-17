7fd638060751e96d93b5d0765d78185697282568
Erik Poort <hello@erikpoort.nl>
2020-02-17 11:51:28 +0100

    Add snapshot pod

---
+++ Pods/Target Support Files/Pods-SnapshotsSnapshotTests/Pods-SnapshotsSnapshotTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SnapshotsSnapshotTests : NSObject
+@end
+@implementation PodsDummy_Pods_SnapshotsSnapshotTests
+@end
---
+++ Pods/Target Support Files/Pods-SnapshotsTests/Pods-SnapshotsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SnapshotsTests : NSObject
+@end
+@implementation PodsDummy_Pods_SnapshotsTests
+@end
